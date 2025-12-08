1be21283c0fa103e0cd83aacbab7277b01a5b63a
xuemin8 <xuemin8@staff.weibo.com>
2025-12-08 18:39:41 +0800

    fix resolve wiki table

--- backend/alembic/versions/c3d4e5f6a7b8_add_wiki_tables.py
+++ backend/alembic/versions/c3d4e5f6a7b8_add_wiki_tables.py
@@ -4,8 +4,8 @@
 
 """Add wiki tables for wiki feature
 
-Revision ID: b2c3d4e5f6a7
-Revises: 2b3c4d5e6f7g
+Revision ID: c3d4e5f6a7b8
+Revises: b2c3d4e5f6a7
 Create Date: 2025-12-03 10:00:00.000000+08:00
 
 """
@@ -16,8 +16,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'b2c3d4e5f6a7'
-down_revision: Union[str, Sequence[str], None] = '2b3c4d5e6f7g'
+revision: str = 'c3d4e5f6a7b8'
+down_revision: Union[str, Sequence[str], None] = 'b2c3d4e5f6a7'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
